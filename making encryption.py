dd06fe7ea02235e4027298126b8e8767ca1cf3f0
Rayonesh <rayanamacarona@gmail.com>
2026-03-14 08:33:07 +0400

    Create making encryption.py

---
+++ making encryption.py
@@ -0,0 +1,7 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+shift = 5
+shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+translation_table = str.maketrans(alphabet, shifted_alphabet)
+text = 'hello world'
+encrypted_text = text.translate(translation_table)
+print(encrypted_text)
